3ef61c4d5948f6456078828cca461c56c67d4d1e
zhangbo98 <17630902721@163.com>
2019-12-23 20:17:09 +0800

    修改bug

--- src/main/java/com/bigdata/Application.java
+++ src/main/java/com/bigdata/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public  static void main(String[]args){
         System.out.print("I like you");
         System.out.print("....");
+        System.out.print("....");
     }
 }
